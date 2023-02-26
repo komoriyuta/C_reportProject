abc9a455893c0ef7c461bbc6a818e75bb5ef6b7f
yutakomori <hamucha565@gmail.com>
2023-02-26 20:28:07 +0900

    main

--- matrixtest.c
+++ matrixtest.c
@@ -1,6 +1,5 @@
-#include "matrixcalculate.h"
+#include <stdio.h>
 #include "xlibcolorgc.h"
-
 void matrixMultiple(float Matrix1[][3], float Matrix2[][3]){
     float _matrixbuf[3][3];
     for(int i=0;i<3;i++){for(int k=0;k<3;k++){_matrixbuf[i][k]=Matrix1[i][k];}}
@@ -24,24 +23,23 @@ void vecMatMultiple(float *Vector1,float Matrix1[][3]){
     }
     
 }
-void vecMatMultiple_elm(float Vec_0,float Vec_1,float Vec_2,float *Vector1,float Matrix1[][3]){
-    float _vecbuf[3]={Vec_0,Vec_1,Vec_2};
-    for (int i = 0;i<3; i++){
-        Vector1[i]=0;
-        for(int k=0;k<3;k++){
-            Vector1[i]+=_vecbuf[k]*Matrix1[i][k];
-        }
+int main(){
+    float  E[3][3]={ {1,0,0},
+                     {0,1,0},
+                     {0,0,1}};
+    float  test[3][3]={{3,4,5},
+                       {5,6,7},
+                       {3,6,2}};
+    float vector[3]={1,2,3};
+    matrixMultiple(test,E);
+    for (size_t i = 0; i < 3; i++){
+        for(int k=0;k<3;k++)
+        printf("%f\n",test[i][k]);
     }
-}
-
-void vecplot(int vector[100][100][3]){
-    for(int i=0;i<100;i++){
-        for(int k=0;k<100;k++){
-            drawPointGC9x9(vector[i][k][0]*5*vector[i][k][0]+240,)
-        }
+    vecMatMultiple(vector,E);
+    printf("\n");
+    for(int i=0;i<3;i++){
+        printf("%f",vector[i]);
     }
+    
 }
-
-void matrixAdd(int *Matrix1, int *Matrix2){}
-void matrixSubtract(int *Matrix1, int *Matrix2){}
-void matScaMultiple(int *Matrix1, int atai){}
